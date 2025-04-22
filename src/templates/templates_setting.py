07477e62e3706b58ce25afe9477a74f9047176c1
iBooLiii <bird23202147@gmail.com>
2025-04-22 14:29:39 +0800

    Create templates_setting.py

---
+++ src/templates/templates_setting.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+# 基本路徑設定
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
